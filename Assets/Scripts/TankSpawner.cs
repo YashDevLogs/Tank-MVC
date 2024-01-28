0a71a7f1ba1eb643d0140fef900adb738dbe9191
Yash <91522919+YashDevLogs@users.noreply.github.com>
2024-01-28 13:32:14 +0530

    Implementation-of-Tank-Movement-using-MVC
    Implemented movement and rotation of the tank.
    implemented camera follow.

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -11,7 +11,7 @@ public class TankSpawner : MonoBehaviour
 
     private void CreateTank()
     {
-        TankModel tankModel = new TankModel();
+        TankModel tankModel = new TankModel(30,30);
         TankController tankController = new TankController(tankView, tankModel);
     }
 
